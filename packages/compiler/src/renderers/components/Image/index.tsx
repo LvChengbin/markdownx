8d1d19d3a60676135ce4b62e5b6d20aa9337b8b9
LvChengbin <lvchengbin59@gmail.com>
2021-06-15 18:51:33 +0800

    feat: support show tooltip instead of slice up text area for tiny iamges

--- packages/compiler/src/renderers/components/Image/index.tsx
+++ packages/compiler/src/renderers/components/Image/index.tsx
@@ -7,12 +7,14 @@
  * Description:
  ******************************************************************/
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import clsx from 'clsx';
 import { Theme } from '@material-ui/core/styles';
 import { makeStyles, createStyles } from '@material-ui/styles';
 import Box from '@material-ui/core/Box';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import Typography from '@material-ui/core/Typography';
+import Tooltip from '@material-ui/core/Tooltip';
 
 export interface ImageProps {
     className?: string;
@@ -76,6 +78,10 @@ const useStyles = makeStyles( ( { spacing }: Theme ) => {
             transition : 'all .3s ease',
             maxHeight : '5em',
             overflow : 'scroll'
+        },
+        loading : {
+            margin : spacing( 1 ),
+            display : 'block'
         }
     } );
 } );
@@ -92,14 +98,71 @@ export default function Image( props: ImageProps ): JSX.Element {
         floatRight = false
     } = props;
 
-    return (
-        <Box component="span"
-            className={clsx( styles.root, className, styles[ align ], inline || styles.block, floatLeft && styles[ 'float-left' ], floatRight && styles[ 'float-right' ] )}
-        >
-            <Box component="span" className={styles.box}>
-                <img className={styles.image} src={src} title={title} alt={alt} width={width} height={height} />
-                <Typography component="span" variant="body2" className={styles.note}>{title}</Typography>
+    const [ isReady, setIsReady ] = useState( false );
+    const [ isTiny, setIsTiny ] = useState( false );
+
+    useEffect( () => {
+        const img = new window.Image();
+        img.onload = (): void => {
+            let displayWidth: number;
+
+            if( typeof width === 'number' ) {
+                displayWidth = width;
+            } else {
+                if( width === 'auto' ) {
+                    displayWidth = img.width;
+                } else if( width.includes( '%' ) ) {
+                    const w = parseInt( width );
+                    displayWidth = isNaN( w ) ? img.width : img.width * w / 100;
+                } else {
+                    const w = parseInt( width );
+                    displayWidth = isNaN( w ) ? img.width : w;
+                }
+            }
+
+            if( displayWidth < 100 ) setIsTiny( true );
+            setIsReady( true );
+        };
+        img.src = src;
+    } );
+
+    return isReady ? (
+        isTiny ? (
+            <Tooltip title={title}>
+                <img
+                    className={clsx(
+                        styles.root,
+                        styles[ align ],
+                        inline || styles.block,
+                        floatLeft && styles[ 'float-left' ],
+                        floatRight && styles[ 'float-right' ]
+                    )}
+                    src={src}
+                    alt={alt}
+                    width={width}
+                    height={height}
+                />
+            </Tooltip>
+        ) : (
+            <Box component="span"
+                className={clsx(
+                    className,
+                    styles.root,
+                    styles[ align ],
+                    inline || styles.block,
+                    floatLeft && styles[ 'float-left' ],
+                    floatRight && styles[ 'float-right' ]
+                )}
+            >
+                <Box component="span" className={styles.box}>
+                    <img className={styles.image} src={src} title={title} alt={alt} width={width} height={height} />
+                    <Typography component="span" variant="body2" className={styles.note}>{title}</Typography>
+                </Box>
             </Box>
+        )
+    ) : (
+        <Box component="span" className={styles.loading}>
+            <CircularProgress size={30} />
         </Box>
     );
 }
